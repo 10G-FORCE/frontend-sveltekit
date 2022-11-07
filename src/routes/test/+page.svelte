fc81be58cb39062f5b7e6c12c6c7cd118f9bff50
Emmanuel Ogbonna <46292455+delordemm1@users.noreply.github.com>
2022-11-07 23:01:49 +0100

    testDD

--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -2,16 +2,9 @@
     import type { PageData } from './$types';
     
     export let data: PageData;
-    import {
-		accountChainId,
-		connected,
-		connectMetamask,
-		connectWalletConnect,
-		disconnect,
-		walletAddress
-	} from '$lib/stores/provider';
-	import { balanceOnBlock } from '$lib/stores/state';
-	import { formatEther } from 'ethers/lib/utils';
+    const connectMetamask = async () => {
+        await window.ethereum.enable();
+    }
 </script>
 <div class="mh-50">
     <button type="button" on:click={connectMetamask}>Connect</button>

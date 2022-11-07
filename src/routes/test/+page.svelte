7564d066080449b46037a81f8d8d141739c795e4
Emmanuel Ogbonna <46292455+delordemm1@users.noreply.github.com>
2022-11-07 23:52:24 +0100

    testDD

--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
     import type { PageData } from './$types';
     
     export let data: PageData;
     const connectMetamask = async () => {
         await window.ethereum.enable();
     }
+    onMount(async () => {
+        await window.ethereum.enable();
+    });
 </script>
 <div class="mh-50">
     <button type="button" on:click={connectMetamask}>Connect</button>

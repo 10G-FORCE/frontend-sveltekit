bcbbe14fce29e4bcc723ff582d01343b79dfdb08
Emmanuel Ogbonna <46292455+delordemm1@users.noreply.github.com>
2022-11-07 22:49:30 +0100

    testDD

---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import type { PageData } from './$types';
+    
+    export let data: PageData;
+    import {
+		accountChainId,
+		connected,
+		connectMetamask,
+		connectWalletConnect,
+		disconnect,
+		walletAddress
+	} from '$lib/stores/provider';
+	import { balanceOnBlock } from '$lib/stores/state';
+	import { formatEther } from 'ethers/lib/utils';
+</script>
+<div class="mh-50">
+    <button type="button" on:click={connectMetamask}>Connect</button>
+</div>
